1227fdc385ea02a1062d4ffc490e496de94b1edf
Tarık <121100943+Yesilyurtarik@users.noreply.github.com>
2023-01-08 15:20:41 +0300

    Add files via upload

---
+++ OS1/src/Proses.java
@@ -0,0 +1,21 @@
+package package1;
+
+//Prosesin ozelliklerini tuttugumuz sinif.
+public class Proses {
+
+	int variszamani;
+	int oncelik;
+	int proseszamani;
+	Process pid;
+	int sayac;
+	boolean basladimi;
+	String renk;
+	int baslatilmazamani;
+
+	public Proses() {
+		sayac = 0;
+		basladimi = false;
+
+		baslatilmazamani = 0;
+	}
+}
