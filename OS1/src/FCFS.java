9be65d255c3c79f9aed83de7b7e17829c86a0956
Ahad Can PAKSOY <73342306+ahadpaksoy@users.noreply.github.com>
2023-01-08 14:48:06 +0300

    Add files via upload

---
+++ OS1/src/FCFS.java
@@ -0,0 +1,29 @@
+package package1;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class FCFS {
+	// FCFS algoritmas�n� uyguladigimiz class
+	public static void schedule(List<Proses> proses) throws InterruptedException {
+		// Sort the processes by their arrival time
+		Collections.sort(proses, new Comparator<Proses>() {
+			public int compare(Proses p1, Proses p2) {
+				return p1.variszamani - p2.variszamani;
+			}
+		});
+
+		int sayac = 0;
+		for (Proses p : proses) {
+			while (p.proseszamani != sayac) {
+				System.out.println(p.oncelik);
+				Thread.sleep(1000);
+				sayac++;
+			}
+			sayac = 0;
+		}
+
+	}
+
+}
